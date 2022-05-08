4b75eb73e9445f192e4f8011ce010027922c65a2
Joshua Coleman <coleman@hey.com>
2022-05-08 17:56:09 +0000

    Add passthrough call for assets and media.

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+// const CleanCSS = require("clean-css");
+
+module.exports = function(eleventyConfig) {
+	// Output directory: _site
+
+	// Copy `media/` to `_site/media`
+	eleventyConfig.addPassthroughCopy("media");
+
+	// Copy `assets` to `_site/assets`
+	eleventyConfig.addPassthroughCopy("assets");
+
+	// Inline Minified CSS
+	// eleventyConfig.addFilter("cssmin", function(code) {
+		// return new CleanCSS({}).minify(code).styles;
+	// });
+};
